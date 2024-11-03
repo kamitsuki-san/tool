01011e458a8ba92df863c254b44453a1ce498848
kamitsuki-san <148661215+kamitsuki-san@users.noreply.github.com>
2024-11-03 11:06:50 +0900

    [kauyomu bookmarklet]format

--- kakuyomu/bookmarklet/countChapter.js
+++ kakuyomu/bookmarklet/countChapter.js
@@ -1,78 +1,105 @@
 // kakuyomu トップページ（https://kakuyomu.jp/my/works/xxxx）にて、各章の合計文字数などを表示する
-javascript:
-(() => {
-    /** メイン処理（合計を計算、設定） */
-    function __cnt () {
-        // 結果を設定するセレクタ（「章とエピソード」の章の行配下）
-        const SUM_IN_CHAPTER = "td:nth-child(5)";
-        const AVG_IN_CHAPTER = "td:nth-child(6)";
-        const AVG2_IN_CHAPTER = "td:last-child";
-    
-        let chapterElement = null;
-        let episodes = [];
-        let episodesInChapter = [];
-        let sumAll = 0;
-        // 全文字数
-        const charAll = parseInt(document.querySelector("#summary-workInfo-characterCount-all").innerText.replace(/[^\d]/g, ""), 10);
-        // 「章とエピソード」の各行に対して実行
-        document.querySelectorAll(".isShown .episode, .isShown .chapter").forEach((element) => {
-            if (element.classList.contains('chapter')) {
-                // 章にあたった時点で、合計情報をリセット
-                chapterElement = element;
-                episodes = [];
-                episodesInChapter.push(episodes);
-                return;
-            }
-            // 以下、エピソードの行に対して処理
-            const epiNum = parseInt(element.querySelector(".episode-characterCount").innerText.replace(/[^\d]/g, ""), 10);
-            episodes.push(epiNum);
-            const sum = episodes.reduce((a, b) => a + b, 0);
-            sumAll += epiNum;
-            const avg = parseInt(sum/episodes.length,10);
-            const l = __rmvHazureVal(episodes);
-            const avg2 = parseInt(l.reduce((a, b) => a + b, 0) / l.length, 10);
-            // エピソード行処理するたび、毎回対応する章に合計を設定している（どのエピソードが章の最後かは意識しない）
-            chapterElement.querySelector(SUM_IN_CHAPTER).innerText=  `合計${sum}`;
-            chapterElement.querySelector(AVG_IN_CHAPTER).innerText = `平均${avg}`;
-            chapterElement.querySelector(AVG2_IN_CHAPTER).innerText = `外れ値除外平均${avg2}`;
-            // 各エピソード行に設定
-            const percent = (sumAll/charAll);
-            element.style.backgroundColor = `rgb(255, 0, 0, ${percent})`;
-            element.querySelector(`.episode-feedback-cheerComment`).innerText = `${sumAll}(${(percent*100).toFixed(1)}%)`;
-        });
-    
-        // 以下は、集計、検討用にコンソールに出力するための処理
-        let allSum = 0;
-        let log = "";
-        document.querySelectorAll(".isShown .chapter").forEach((element, index) => {
-            const sum = parseInt(element.querySelector(SUM_IN_CHAPTER).innerText.replace(/[^\d]/g, ""), 10);
-            const avg = parseInt(element.querySelector(AVG_IN_CHAPTER).innerText.replace(/[^\d]/g, ""), 10);
-            const avg2 = parseInt(element.querySelector(AVG2_IN_CHAPTER).innerText.replace(/[^\d]/g, ""), 10);
-            log += (`${index+1}\t${sum}\t${avg}\t${avg2}\n`);
-            allSum += sum;
-        });
-        log = ("全合計" + allSum + "\n") + log;
-        // 各行
-        log += "各行\n";
-        let sum = 0;
-        episodesInChapter.forEach((episodes, chapter) => {
-            episodes.forEach((epiCharNum, epiNum) => {
-                sum += epiCharNum;
-                log += `${chapter+1}\t${epiNum+1}\t${epiCharNum}\t${sum}\n`;
-            });
-        });
-        console.log(log);
-    };
-    /** 引数の配列から、外れ値を除外した配列を返す */
-    function __rmvHazureVal (numList) {
-        numList = numList.sort();
-        // 3分割して中央を取得
-        const result = numList.slice(parseInt(numList.length/3, 10), -parseInt(numList.length/3, 10));
-        return result;
-    };
+javascript: (() => {
+  /** メイン処理（合計を計算、設定） */
+  function __cnt() {
+    // 結果を設定するセレクタ（「章とエピソード」の章の行配下）
+    const SUM_IN_CHAPTER = "td:nth-child(5)";
+    const AVG_IN_CHAPTER = "td:nth-child(6)";
+    const AVG2_IN_CHAPTER = "td:last-child";
 
-    /****** 実行 *****/
-    __cnt();
+    let chapterElement = null;
+    let episodes = [];
+    let episodesInChapter = [];
+    let sumAll = 0;
+    // 全文字数
+    const charAll = parseInt(
+      document
+        .querySelector("#summary-workInfo-characterCount-all")
+        .innerText.replace(/[^\d]/g, ""),
+      10
+    );
+    // 「章とエピソード」の各行に対して実行
+    document
+      .querySelectorAll(".isShown .episode, .isShown .chapter")
+      .forEach((element) => {
+        if (element.classList.contains("chapter")) {
+          // 章にあたった時点で、合計情報をリセット
+          chapterElement = element;
+          episodes = [];
+          episodesInChapter.push(episodes);
+          return;
+        }
+        // 以下、エピソードの行に対して処理
+        const epiNum = parseInt(
+          element
+            .querySelector(".episode-characterCount")
+            .innerText.replace(/[^\d]/g, ""),
+          10
+        );
+        episodes.push(epiNum);
+        const sum = episodes.reduce((a, b) => a + b, 0);
+        sumAll += epiNum;
+        const avg = parseInt(sum / episodes.length, 10);
+        const l = __rmvHazureVal(episodes);
+        const avg2 = parseInt(l.reduce((a, b) => a + b, 0) / l.length, 10);
+        // エピソード行処理するたび、毎回対応する章に合計を設定している（どのエピソードが章の最後かは意識しない）
+        chapterElement.querySelector(SUM_IN_CHAPTER).innerText = `合計${sum}`;
+        chapterElement.querySelector(AVG_IN_CHAPTER).innerText = `平均${avg}`;
+        chapterElement.querySelector(
+          AVG2_IN_CHAPTER
+        ).innerText = `外れ値除外平均${avg2}`;
+        // 各エピソード行に設定
+        const percent = sumAll / charAll;
+        element.style.backgroundColor = `rgb(255, 0, 0, ${percent})`;
+        element.querySelector(
+          `.episode-feedback-cheerComment`
+        ).innerText = `${sumAll}(${(percent * 100).toFixed(1)}%)`;
+      });
+
+    // 以下は、集計、検討用にコンソールに出力するための処理
+    let allSum = 0;
+    let log = "";
+    document.querySelectorAll(".isShown .chapter").forEach((element, index) => {
+      const sum = parseInt(
+        element.querySelector(SUM_IN_CHAPTER).innerText.replace(/[^\d]/g, ""),
+        10
+      );
+      const avg = parseInt(
+        element.querySelector(AVG_IN_CHAPTER).innerText.replace(/[^\d]/g, ""),
+        10
+      );
+      const avg2 = parseInt(
+        element.querySelector(AVG2_IN_CHAPTER).innerText.replace(/[^\d]/g, ""),
+        10
+      );
+      log += `${index + 1}\t${sum}\t${avg}\t${avg2}\n`;
+      allSum += sum;
+    });
+    log = "全合計" + allSum + "\n" + log;
+    // 各行
+    log += "各行\n";
+    let sum = 0;
+    episodesInChapter.forEach((episodes, chapter) => {
+      episodes.forEach((epiCharNum, epiNum) => {
+        sum += epiCharNum;
+        log += `${chapter + 1}\t${epiNum + 1}\t${epiCharNum}\t${sum}\n`;
+      });
+    });
+    console.log(log);
+  }
+  /** 引数の配列から、外れ値を除外した配列を返す */
+  function __rmvHazureVal(numList) {
+    numList = numList.sort();
+    // 3分割して中央を取得
+    const result = numList.slice(
+      parseInt(numList.length / 3, 10),
+      -parseInt(numList.length / 3, 10)
+    );
+    return result;
+  }
+
+  /****** 実行 *****/
+  __cnt();
 })();
 
 // https://crocro.com/tools/item/gen_bookmarklet/
--- kakuyomu/bookmarklet/createTweet.js
+++ kakuyomu/bookmarklet/createTweet.js
@@ -1,30 +1,36 @@
 // kakuyomu エピソード編集ページ（https://kakuyomu.jp/my/works/xxxx/episodes/xxxx）から、ツイート用テキストを作成する
-javascript:
-(() => {
-    /** メイン処理 */
-    function __main () {
-        /** 結果を設定する先の親要素セレクタ */ 
-        const RESULT_PARENT = "#contentAside-inner";
-        /** 結果HTMLのテンプレート（xxxxを置き換え） */
-        const RESULT_HTML_TEMPLATE = `<div style="border:1px solid black;">xxxx</div>`;
-    
-        const workTitle = document.getElementById("workTitle-title").getAttribute("title");
-        const episodeTitle = document.getElementById("contentMainHeader-pageTitle-episodeTitle").innerText;
-        const url = document.location.href.replace("/my/", "/");
-        const episodeNumber = document.getElementById("workTitle-episodeNumber").innerText.replace(/[^\d]/g, "");
+javascript: (() => {
+  /** メイン処理 */
+  function __main() {
+    /** 結果を設定する先の親要素セレクタ */
+    const RESULT_PARENT = "#contentAside-inner";
+    /** 結果HTMLのテンプレート（xxxxを置き換え） */
+    const RESULT_HTML_TEMPLATE = `<div style="border:1px solid black;">xxxx</div>`;
 
-        const postMain = `「${workTitle}」<br>${episodeTitle}<br><br><br>` + 
-        `#narou #小説家になろう https://ncode.syosetu.com/xxxx  /${episodeNumber}<br>` + 
-        `#kakuyomu #カクヨム ${url}`;
-        const post = RESULT_HTML_TEMPLATE.replace("xxxx", postMain);
+    const workTitle = document
+      .getElementById("workTitle-title")
+      .getAttribute("title");
+    const episodeTitle = document.getElementById(
+      "contentMainHeader-pageTitle-episodeTitle"
+    ).innerText;
+    const url = document.location.href.replace("/my/", "/");
+    const episodeNumber = document
+      .getElementById("workTitle-episodeNumber")
+      .innerText.replace(/[^\d]/g, "");
 
-        let div = document.createElement('div');
-        div.innerHTML = post;
-        document.querySelector(RESULT_PARENT).appendChild(div);
-    };
+    const postMain =
+      `「${workTitle}」<br>${episodeTitle}<br><br><br>` +
+      `#narou #小説家になろう https://ncode.syosetu.com/xxxx  /${episodeNumber}<br>` +
+      `#kakuyomu #カクヨム ${url}`;
+    const post = RESULT_HTML_TEMPLATE.replace("xxxx", postMain);
 
-    /****** 実行 *****/
-    __main();
+    let div = document.createElement("div");
+    div.innerHTML = post;
+    document.querySelector(RESULT_PARENT).appendChild(div);
+  }
+
+  /****** 実行 *****/
+  __main();
 })();
 
 // https://crocro.com/tools/item/gen_bookmarklet/
